66d30aeb16b1d86d99b78151e7a2fca1342a9543
doctor <18632674212@163.com>
2019-05-22 15:29:03 +0800

    sum function

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'lib/index.js'
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js'
   }
 }
